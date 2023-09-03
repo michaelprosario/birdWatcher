63c4270ddb179e18b864162ab1245c65a794a243
michaelprosario <michael.p.rosario@gmail.com>
2023-09-03 15:41:58 +0000

    feat ( object detector ) got it working

--- object-detection.js
+++ object-detection.js
@@ -2,33 +2,30 @@ const tf = require("@tensorflow/tfjs-node")
 
 const cocosSSd = require("@tensorflow-models/coco-ssd")
 const fs = require("fs");
+const { createCanvas, loadImage } = require('canvas');
 
-let ObjectDetection = class {
+let ObjectDetection = class 
+{
     static async predict(image)
     {
         let model = await cocosSSd.load();
      
-        const imageBuffer = await fs.readFileSync(image);
-        const imageArray = tf.node.decodeImage(imageBuffer);
+        const img = await loadImage(image);
+        const canvas = createCanvas(img.width, img.height);
+        const ctx = canvas.getContext('2d');
+        ctx.drawImage(img, 0, 0);
+      
+        const predictions = await model.detect(canvas);
         
-        // Reshape the image into a 3D tensor (height x width x channels)
-        const height = imageArray.shape[0];
-        const width = imageArray.shape[1];
-        const channels = imageArray.shape[2];
-        const tensor3D = tf.reshape(imageArray, [height, width, channels]);
-
-        
-        let predictions = await model.detect(tensor3D);
         return { predictions: predictions }
     }
 }
 
-async function main(){
-
+async function main()
+{
     let image = "birds.png";
     result = await ObjectDetection.predict(image);
     console.log(result)
-
 }
 
 main();
