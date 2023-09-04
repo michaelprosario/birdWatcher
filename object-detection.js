5ba5505920606cbe9d664db5930ef1391adfa6d0
michael_rosario <michael.rosario@designinteractive.net>
2023-09-03 21:11:20 -0400

    feat ( object detector ) small refactor

--- object-detection.js
+++ object-detection.js
@@ -1,30 +1,41 @@
 const tf = require("@tensorflow/tfjs-node")
 
 const cocosSSd = require("@tensorflow-models/coco-ssd")
-const fs = require("fs");
 const { createCanvas, loadImage } = require('canvas');
 
-let ObjectDetection = class 
+class ObjectDetection 
 {
-    static async predict(image)
+    constructor()
     {
-        let model = await cocosSSd.load();
-     
+        this.model = null;
+    }
+
+    async predict(image)
+    {
+        if(!this.model)
+        {
+            this.model = await cocosSSd.load();
+        }
+
+        const canvas = await this.makeCanvasFromFilePath(image);    
+        const predictions = await this.model.detect(canvas);
+        
+        return { predictions: predictions }
+    }
+
+    async makeCanvasFromFilePath(image) {
         const img = await loadImage(image);
         const canvas = createCanvas(img.width, img.height);
         const ctx = canvas.getContext('2d');
         ctx.drawImage(img, 0, 0);
-      
-        const predictions = await model.detect(canvas);
-        
-        return { predictions: predictions }
+        return canvas;
     }
 }
 
 async function main()
 {
     let image = "birds.png";
-    result = await ObjectDetection.predict(image);
+    result = await new ObjectDetection().predict(image);
     console.log(result)
 }
 
